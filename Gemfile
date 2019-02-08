043d6eed9f90868eab8040980187ee7dfe90e4c1
Daniel Hernandez <elreymcd@yahoo.com>
2019-02-07 20:27:44 -0600

    add session

--- Gemfile
+++ Gemfile
@@ -26,6 +26,10 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
+gem 'dotenv-rails'
+gem 'bootstrap', '~> 4.1', '>= 4.1.3'
+gem 'jquery-rails'
+gem 'pry'
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+    before_action :require_log_in, except: [:new, :create, :index]
+
+    def index
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user.try(:authenticate, params[:user][:password])
+            session[:user_id] = @user.id
+            session[:position] = @user.position
+            session[:user_name] = @user.first_name + " " + @user.last_name
+            redirect_to @user, notice: 'User was successfully Logged In'
+        else
+            flash.alert = "Username and/or Password Not Found"
+            redirect_to signin_path, notice: 'User Not Found'
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+        redirect_to '/'
+    end
+
+end
