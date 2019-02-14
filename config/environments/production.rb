f25267417d8f935494994ebdef8c89f000232a6b
Daniel Hernandez <elreymcd@yahoo.com>
2019-02-14 16:15:00 -0600

    olf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,14 +89,14 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  # Do not dump schema after migrations.
+  # Do not dump schema after migrations. 587 was the old port
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port: 587,
+    port: ENV["PORT"],
     domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
